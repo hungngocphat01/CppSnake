3ad0dd2ec5cdb68acadf2889ca861cc7a48150cb
phathung2001 <phathung2001@gmail.com>
2020-08-17 22:45:29 +0700

    Border to be customizable

--- gui.h
+++ gui.h
@@ -2,6 +2,8 @@
 #define TEXTURE_MAX_HEIGHT 12
 #define TEXTURE_MAX_WIDTH 70
 
+
+
 void pauseProg() {
     nodelay(stdscr, FALSE);	
     getch();
@@ -10,15 +12,15 @@ void pauseProg() {
 
 void drawFrame() {
     // Draw vertical bars
-    for (unsigned i = 0; i < SHEIGHT; i++) {
-        mvprintw(i, 0, "#");
-        mvprintw(i, SWIDTH - 1, "#");
+    for (unsigned i = 0; i < SCR_HEIGHT; i++) {
+        mvprintw(i, 0, borderc);
+        mvprintw(i, SCR_WIDTH - 1, borderc);
     }
 
     // Draw horizontal bars
-    for (unsigned i = 0; i < SWIDTH; i++) {
-        mvprintw(0, i, "#");
-        mvprintw(SHEIGHT - 1, i, "#");
+    for (unsigned i = 0; i < SCR_WIDTH; i++) {
+        mvprintw(0, i, borderc);
+        mvprintw(SCR_HEIGHT - 1, i, borderc);
     }
 }
 
@@ -26,7 +28,7 @@ inline void guiInit() {
     initscr();
 	noecho();
 	curs_set(FALSE);
-    getmaxyx(stdscr, SHEIGHT, SWIDTH);	
+    getmaxyx(stdscr, SCR_HEIGHT, SCR_WIDTH);	
 
     // Disable getch() wait
     cbreak();
@@ -60,8 +62,8 @@ void printFromFile(const char* filename) {
         }
     }
 
-    unsigned x = SWIDTH/2 - max_width/2;
-    unsigned y = SHEIGHT/2 - lines/2;
+    unsigned x = SCR_WIDTH/2 - max_width/2;
+    unsigned y = SCR_HEIGHT/2 - lines/2;
 
     for (unsigned i = 0; i <= lines; i++) {
         mvprintw(y++, x, lbuffer[i]);
@@ -89,11 +91,11 @@ inline void gameOver() {
 }
 
 void checkTermSize() {
-    while (SWIDTH < MINW || SHEIGHT < MINH) {
+    while (SCR_WIDTH < MINW || SCR_HEIGHT < MINH) {
         clear();
-		mvprintw(0, 0, "Your terminal size is insufficent to play the game.\nResize it to at least %dx%d (WxH) and press any key to continue.\nCurrent size: %dx%d", MINW, MINH, SWIDTH, SHEIGHT);
+		mvprintw(0, 0, "Your terminal size is insufficent to play the game.\nResize it to at least %dx%d (WxH) and press any key to continue.\nCurrent size: %dx%d", MINW, MINH, SCR_WIDTH, SCR_HEIGHT);
         refresh();
         pauseProg();
-        getmaxyx(stdscr, SHEIGHT, SWIDTH);	
+        getmaxyx(stdscr, SCR_HEIGHT, SCR_WIDTH);	
 	}
 }
