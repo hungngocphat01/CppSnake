b2ec11a6f05b135cffe20a8a3570e3d2a694d0dc
phathung2001 <phathung2001@gmail.com>
2020-08-17 22:45:49 +0700

    Add argument handlers, interval to be customizable

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "declarations.h"
 #include "snake.h"
 #include "gui.h"
+#include "arguments.h"
 
-int SHEIGHT, SWIDTH;
+int SCR_HEIGHT, SCR_WIDTH;
 bool GAMEOVER = false;
+int delaytime = 300;
 
-int main () {
-	// Initialize
+int main (int argc, char** argv) {
+	argp_parse(&Argp, argc, argv, 0, 0, 0);
+
+	// Initialize GUI
 	guiInit();
 	checkTermSize();
 	srand(time(NULL));
@@ -16,20 +20,20 @@ int main () {
 	int keyread;
 
 	snake s;
-	s.head = {SWIDTH/2, SHEIGHT/2};
+	s.head = {SCR_WIDTH/2, SCR_HEIGHT/2};
 	s.genfood();
 	s.drawfood();
 	refresh();
 	
 	const char* guidestr = "Press P to pause, Q to quit.";
 	clear();
-	mvprintw(SHEIGHT/2 - 1, SWIDTH/2 - strlen(guidestr)/2 + 1, guidestr);
+	mvprintw(SCR_HEIGHT/2 - 1, SCR_WIDTH/2 - strlen(guidestr)/2 + 1, guidestr);
 	refresh();
 	napms(1000);
 
 	while (1) {
 		checkTermSize();
-		getmaxyx(stdscr, SHEIGHT, SWIDTH);
+		getmaxyx(stdscr, SCR_HEIGHT, SCR_WIDTH);
 		keyread = getch();
 
 		// Easter egg
@@ -49,12 +53,12 @@ int main () {
 		s.drawfood();
 		s.drawsnake();
 		
-		// mvprintw(SHEIGHT - 1, 0, "px=%d py=%d", s.prey.x, s.prey.y);
-		// mvprintw(SHEIGHT - 2, 0, "x=%d y=%d", s.head.x, s.head.y);
+		// mvprintw(SCR_HEIGHT - 1, 0, "px=%d py=%d", s.prey.x, s.prey.y);
+		// mvprintw(SCR_HEIGHT - 2, 0, "x=%d y=%d", s.head.x, s.head.y);
 		// mvprintw(my - 3, 0, "getch=%c", direction);
 		// mvprintw(my - 4, 0, "len=%d", s.len);
 		refresh();
-		napms(200);
+		napms(delaytime);
 	}	
 	gameOver();
 	endwin();
