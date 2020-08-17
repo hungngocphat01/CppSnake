a790d637ef0a787962ac39cbd5ca8cd29b16d003
phathung2001 <phathung2001@gmail.com>
2020-08-17 22:45:14 +0700

    Add argument handler global vars

--- declarations.h
+++ declarations.h
@@ -1,11 +1,20 @@
 #include <ncurses.h>
-#include <cstring>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
 
-extern int SHEIGHT, SWIDTH;
+#ifdef __APPLE__
+    // Must "brew install argp-standalone" first
+    #include "/usr/local/include/argp.h"
+#else
+    #include <argp.h>
+#endif
+
+extern int SCR_HEIGHT, SCR_WIDTH;
 extern bool GAMEOVER;
+extern char bodyc[2], headc[2], borderc[2], foodc[2];
+extern int delaytime;
 
 #define MINW 70
 #define MINH 20
