1af6929a1459e0d65155d0c20eadd5c0dead1c52
phathung2001 <phathung2001@gmail.com>
2020-08-21 22:29:16 +0700

    Standardize int size, add exception

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include "gui.h"
 #include "arguments.h"
 
-int SCR_HEIGHT, SCR_WIDTH;
+int32_t SCR_HEIGHT, SCR_WIDTH;
 bool GAMEOVER = false;
-int DELAYTIME = 300;
+int32_t DELAYTIME = 300;
 char LOADFILENAME[255];
 
 int main (int argc, char** argv) {
@@ -16,69 +16,76 @@ int main (int argc, char** argv) {
 	checkTermSize();
 	checkCanvas();
 	srand(time(NULL));
-
-	printMenu();
-
-	int keyread;
-
 	snake s;
-	if (strlen(LOADFILENAME) != 0) {
-		if(!s.read(LOADFILENAME)){
-			endwin();
-			return -1;
+
+	try {
+		printMenu();
+
+		int keyread;
+
+		if (strlen(LOADFILENAME) != 0) {
+			if(!s.read(LOADFILENAME)){
+				endwin();
+				return -1;
+			}
+			else {
+				SCORE = (s.len - 1) * 10;
+			}
 		}
 		else {
-			SCORE = (s.len - 1) * 10;
+			s.head = {SCR_WIDTH/2, SCR_HEIGHT/2};
+			s.head = {SCR_WIDTH/2, SCR_HEIGHT/2};
+			s.genfood();
 		}
-	}
-	else {
-		s.head = {SCR_WIDTH/2, SCR_HEIGHT/2};
-		s.head = {SCR_WIDTH/2, SCR_HEIGHT/2};
-		s.genfood();
-	}
-	s.drawfood();
-	refresh();
-	
-	const char* guidestr = "Press P to pause, Q to quit, O to save.";
-	clear();
-	mvprintw(SCR_HEIGHT/2 - 1, SCR_WIDTH/2 - strlen(guidestr)/2 + 1, guidestr);
-	refresh();
-	napms(2000);
-
-	while (1) {
-		checkTermSize();
-		checkCanvas();
-		getmaxyx(stdscr, SCR_HEIGHT, SCR_WIDTH);
-		keyread = getch();
-
-		// Easter egg
-		if (keyread == '/') {
-			if (DEBUGMODE)
-				s.grow();
-		}
-		else if (keyread == 'q' || GAMEOVER) {
-			break;
-		}
-		else if (keyread == 'p') {
-			pauseGame();
-		}
-		else if (keyread == 'o') {
-			showSavePrompt(s);
-		}
-
-		clear();
-		drawFrame();
-		s.move(keyread);
 		s.drawfood();
-		s.drawsnake();
-		
-		// mvprintw(SCR_HEIGHT - 2, 0, "x=%d y=%d", s.head.x, s.head.y);
-		// mvprintw(my - 3, 0, "getch=%c", direction);
-		// mvprintw(my - 4, 0, "len=%d", s.len);
 		refresh();
-		napms(DELAYTIME);
-	}	
-	gameOver();
+		
+		const char* guidestr = "Press P to pause, Q to quit, O to save.";
+		clear();
+		mvprintw(SCR_HEIGHT/2 - 1, SCR_WIDTH/2 - strlen(guidestr)/2 + 1, guidestr);
+		refresh();
+		napms(2000);
+
+		while (1) {
+			checkTermSize();
+			checkCanvas();
+			getmaxyx(stdscr, SCR_HEIGHT, SCR_WIDTH);
+			keyread = getch();
+
+			// Easter egg
+			if (keyread == '/') {
+				if (DEBUGMODE)
+					s.grow();
+			}
+			else if (keyread == 'q' || GAMEOVER) {
+				break;
+			}
+			else if (keyread == 'p') {
+				pauseGame();
+			}
+			else if (keyread == 'o') {
+				showSavePrompt(s);
+			}
+
+			clear();
+			drawFrame();
+			s.move(keyread);
+			s.drawfood();
+			s.drawsnake();
+			
+			// mvprintw(SCR_HEIGHT - 2, 0, "x=%d y=%d", s.head.x, s.head.y);
+			// mvprintw(my - 3, 0, "getch=%c", direction);
+			// mvprintw(my - 4, 0, "len=%d", s.len);
+			refresh();
+			napms(DELAYTIME);
+		}	
+	}
+	catch (const std::exception& e) {
+		mvprintw(0, 0, "An error occurred: %s", e.what());
+		pauseProg();
+	}
+
+	free(s.body);
 	endwin();
 	return 0;
 }
